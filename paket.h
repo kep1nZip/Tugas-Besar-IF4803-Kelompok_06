172f8cc0271bea5a29b684dc254b9471c1801983
Zyco-tech <zidanemardico@gmail.com>
2025-12-07 11:55:45 +0700

    modifikasi paket

--- paket.h
+++ paket.h
@@ -7,7 +7,24 @@
 using namespace std;
 
 
+// CREATE
+adrChild createElemenChild(infotypeChild x);
 
+// INSERT
+void insertFirstChild(adrParent &p, adrChild c);
+void insertLastChild(adrParent &p, adrChild c);
+void insertAfterChild(adrParent &p, adrChild prec, adrChild c);
+
+// DELETE
+void deleteFirstChild(adrParent &p, adrChild &c);
+void deleteLastChild(adrParent &p, adrChild &c);
+void deleteAfterChild(adrParent &p, adrChild prec, adrChild &c);
+
+// SEARCH
+adrChild findElemenChild(adrParent p, int idPaket);
+
+// VIEW
+void viewChild(adrParent p);
 
 
 #endif /* PAKET_H_INCLUDED */
