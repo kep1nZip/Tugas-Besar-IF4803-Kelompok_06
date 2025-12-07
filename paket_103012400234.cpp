765c6f7532aaac3bfc3496abd1ad5dcdc92567d8
Zyco-tech <zidanemardico@gmail.com>
2025-12-07 11:58:58 +0700

    nambahin function

--- paket_103012400234.cpp
+++ paket_103012400234.cpp
@@ -2,4 +2,49 @@
 #include <iostream>
 using namespace std;
 
+adrChild createElemenChild(infotypeChild x){
+    adrChild c = new elmChild;
+    c->info = x;
+    c->next = nullptr;
+    return c;
+}
 
+//INSERT FIRST
+void insertFirstChild(adrParent &p, adrChild c){
+   c->next = p->firstChild;
+   p->firstChild = c;
+}
+
+//INSERT LAST
+void insertLastChild(adrParent &p, adrChild c){
+    if (p->firstChild == nullptr){
+        p->firstChild = c;
+    }else{
+        adrChild q = p->firstChild;
+        while(q->next != nullptr){
+            q = q->next;
+        }
+        q->next = c;
+    }
+}
+void insertAfterChild(adrParent &p, adrChild prec, adrChild c){
+    if (prec == nullptr) {
+        insertFirstChild(p, c);
+    } else {
+        c->next = prec->next;
+        prec->next = c;
+    }
+}
+
+//DELETE FIRST
+void deleteFirstChild(adrParent &p, adrChild &c){
+    if(p->firstChild == nullptr){
+        c = nullptr;
+    }else{
+        c = p->firstChild;
+        p->firstChild = c->next;
+        c->next = nullptr;
+
+    }
+
+}
